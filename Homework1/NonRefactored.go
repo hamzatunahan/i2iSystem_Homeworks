5d875f31c2ba5cca6753da7d33ec4d1c67047a02
Hamza Tunahan ARSLAN <hamzarslan34@gmail.com>
2022-07-26 22:48:38 +0300

    Homework 1

---
+++ Homework1/NonRefactored.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	f, err := os.Open("sayilar.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		var text string = scanner.Text() // text değişkeni sayılar.txt dosyasındaki her satırın değerini alır
+		deger, err := strconv.Atoi(text) // string degeri integer degerine donusturur
+		var sayac int = 0
+
+		if err != nil { // hata varsa
+			panic(err)
+		}
+
+		for i := 2; i <= int(math.Floor(float64(deger)/2)); i++ {
+			if deger%i == 0 {
+				sayac++
+			}
+		}
+		if sayac == 0 {
+			fmt.Println(deger, "Prime number")
+		} else {
+			fmt.Println(deger, "Prime number degil")
+		}
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ Homework1/Refactored.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	var allnumber []string = readFile()
+	IsNumberPrimee(allnumber)
+}
+
+func IsNumberPrimee(value []string) {
+	var sayac int = 0
+
+	for i := 0; i < len(value); i++ {
+		sayac = 0
+		deger, err := strconv.Atoi(value[i]) // string degeri integer degerine donusturur
+		if err != nil {
+			panic(err)
+		}
+		for i := 2; i <= int(math.Floor(float64(deger)/2)); i++ {
+			if deger%i == 0 {
+				sayac++
+			}
+		}
+		if sayac == 0 {
+			fmt.Println(deger, "Prime number")
+		} else {
+			fmt.Println(deger, "Prime number degil")
+		}
+
+	}
+}
+
+func readFile() []string {
+	readFile, err := os.Open("sayilar.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	var fileLines []string
+	//fileLines := []string{}
+	for fileScanner.Scan() {
+		fileLines = append(fileLines, fileScanner.Text())
+	}
+
+	readFile.Close()
+	return fileLines
+}
